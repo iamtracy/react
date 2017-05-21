ac210f73a44c7e8ef81ad508ec5363eb0c22ab5e
Tracy Lee <tlee@inxpo.com>
2017-05-21 13:14:33 -0500

    init

---
+++ test/components/app-test.js
@@ -0,0 +1,11 @@
+import { renderComponent, expect } from '../test-helper';
+import App from '../../src/components/app';
+
+describe('App', () => {
+    let component;
+
+    beforeEach(() => {
+        component = renderComponent(App);
+    });
+
+});
